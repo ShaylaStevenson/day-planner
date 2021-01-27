fbc8f03ed9b37d45cb3da9ac2d0762e34c5f68dd
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-01-26 17:54:17 -0800

    if statement to adjust colors of textarea

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,32 @@ $(document).ready(function() {
         var currentDate = moment().format('dddd MMM DD[,] YYYY');
         $('#currentDay').text(currentDate);
     }
+    getTodaysDate();
+
     //will be used to check if hour is in past or future
     var currentTime = moment().format("H");
     console.log(currentTime);
+
+    var hourRowsArray = [];
+    hourRowsArray = $(".row");
+    console.log(hourRowsArray);
+
+    //assign classes to past, present, future using the value attribute
+    $(".description").each(function() {
+        var value = parseInt($(this).attr("value"));
+
+        if (value < currentTime) {
+            $(this).addClass("past");
+        }
+        else if (value == currentTime) {
+            $(this).addClass("present")
+        }
+        else {
+            $(this).addClass("future")
+        }
+    });
+    
+
 
 
 
