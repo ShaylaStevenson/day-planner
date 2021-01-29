099625e7717a72ff2c2e08eed2c00b0fd091e4cd
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-01-28 16:24:45 -0800

    refactored JS-removed 20 lines:)

--- assets/script.js
+++ assets/script.js
@@ -8,34 +8,18 @@ $(document).ready(function() {
     getTodaysDate();
 
     //will be used to check if hour is in past or future
-    var currentTime = 13;
-    //moment().format("H");
-
-    //save text input to variables
-    var nineEvents = $("#nine");
-    var tenEvents = $("#ten");
-    var elevenEvents = $("#eleven");
-    var twelveEvents = $("#twelve");
-    var oneEvents = $("#one");
-    var twoEvents = $("#two");
-    var threeEvents = $("#three");
-    var fourEvents = $("#four");
-    var fiveEvents = $("#five");
+    var currentTime = moment().format("H");
 
     //get previously stored events to display in corresponding row
-    nineEvents.append(localStorage.getItem("9"));
-    tenEvents.append(localStorage.getItem("10"));
-    elevenEvents.append(localStorage.getItem("11"));
-    twelveEvents.append(localStorage.getItem("12"));
-    oneEvents.append(localStorage.getItem("1"));
-    twoEvents.append(localStorage.getItem("2"));
-    threeEvents.append(localStorage.getItem("3"));
-    fourEvents.append(localStorage.getItem("4"));
-    fiveEvents.append(localStorage.getItem("5"));
+    $(".row").each(function() {
+        var time = $(this).attr("id");
+        var textArea = localStorage.getItem(time);
+        $(this).find("textarea").val(textArea);
+    })
 
-    //assign classes to past, present, future using the value attribute
-    $(".description").each(function() {
-        var value = parseInt($(this).attr("value"));
+    //assign classes to past, present, future using the main div's ID attribute
+    $(".row").each(function() {
+        var value = parseInt($(this).attr("id"));
 
         if (value < currentTime) {
             $(this).addClass("past");
@@ -50,14 +34,8 @@ $(document).ready(function() {
 
     //set user's input to storage onclick
     $(".saveBtn").on("click", function() {
-        localStorage.setItem("9", (nineEvents.val()));
-        localStorage.setItem("10", (twoEvents.val()));
-        localStorage.setItem("11", (elevenEvents.val()));
-        localStorage.setItem("12", (twelveEvents.val()));
-        localStorage.setItem("1", (oneEvents.val()));
-        localStorage.setItem("2", (twoEvents.val()));
-        localStorage.setItem("3", (threeEvents.val()));
-        localStorage.setItem("4", (fourEvents.val()));
-        localStorage.setItem("5", (fiveEvents.val()));
+        var time = $(this).parent().attr("id");
+        var textArea = $(this).siblings(".description").val();
+        localStorage.setItem(time, textArea);
     })
 })
